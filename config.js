5c6d6d481f84cfe56f3f93926deb02173cb68ca3
Michael <michaelbrianjean@gmail.com>
2017-01-15 10:12:12 -0500

    Fixed .gitignore.

--- config.js
+++
@@ -1,8 +0,0 @@
-var config = {};
- 
-config.db = {};
- 
-config.db.host = "fccuser:fccuserpass1!@ds111489.mlab.com:11489";
-config.db.name = "url-shortener-microservice";
- 
-module.exports = config;
