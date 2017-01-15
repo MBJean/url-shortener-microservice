d986f1946329a23a943fab1ce81d4fd2474d2cf5
Michael <michaelbrianjean@gmail.com>
2017-01-15 10:03:59 -0500

    Adjusted config and .gitignore.

---
+++ config.js
@@ -0,0 +1,8 @@
+var config = {};
+ 
+config.db = {};
+ 
+config.db.host = "fccuser:fccuserpass1!@ds111489.mlab.com:11489";
+config.db.name = "url-shortener-microservice";
+ 
+module.exports = config;
